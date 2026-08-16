be3c2ba5b915d14e4a1818eabc1331072507d840
Tobias-Svendsen <124564285+Tobias-Svendsen@users.noreply.github.com>
2026-08-16 22:26:23 +0200

    Declare trainer randomizer hook

--- include/pokemon_randomizer.h
+++ include/pokemon_randomizer.h
@@ -4,6 +4,8 @@
 #include "global.h"
 #include "constants/species.h"
 
+struct Pokemon;
+
 /* Randomizer domains are deliberately independent so adding/removing a wild
  * encounter does not reshuffle trainer or boss results. */
 enum PokemonRandomizerDomain
@@ -17,5 +19,6 @@ enum PokemonRandomizerDomain
 void PokemonRandomizer_Init(u32 trainerId);
 u32 PokemonRandomizer_GetSeed(enum PokemonRandomizerDomain domain, u32 index);
 enum Species PokemonRandomizer_GetSpecies(enum PokemonRandomizerDomain domain, u32 index);
+void PokemonRandomizer_RandomizeTrainerParty(struct Pokemon *party, u16 trainerId);
 
 #endif
